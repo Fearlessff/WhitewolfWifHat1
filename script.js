fe6c1b4fcb09a28283a8169a6e06d198b5adeac9
NITRO_OG <137004224+Fearlessff@users.noreply.github.com>
2026-02-07 19:31:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Snow + eye tracking
-const eyes=document.querySelector('.eyes');
-document.addEventListener('mousemove',e=>{
-if(window.innerWidth>768){
-eyes.style.left=e.clientX/50+'px';
-eyes.style.top=e.clientY/50+'px';
-}
-});
